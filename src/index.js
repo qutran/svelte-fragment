46bf214d25c3b8159ae0ab1fe652d1a52ab670f4
Swizz <swizz@users.noreply.github.com>
2020-01-07 11:29:22 +0100

    Use adoption to remove template node on use

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
 export default function fragment(node) {
-  node.parentElement.appendChild(node.content);
-  node.setAttribute('style', 'display: none;');
-
-  return {
-    destroy() {
-      if (
-        node &&
-        node.parentElement &&
-        node.parentNode.contains(node.content)
-      ) {
-        node.parentElement.removeChild(node.content);
-      }
-    },
-  };
+  node.parentElement.appendChild(document.adoptNode(node).content);
 }
