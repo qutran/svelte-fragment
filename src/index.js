aacbdd537d42eec885bfd05e868345eb4ea487d2
Swizz <swizz@users.noreply.github.com>
2020-01-07 13:16:47 +0100

    Fix fragment children removal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 export default function fragment(node) {
-  node.parentElement.appendChild(document.adoptNode(node).content);
+  // We need to keep the reference of the parent because of the adoption
+  const parent = node.parentElement;
+
+  // As appening a fragment as node child remove its content, we need to save the
+  // children references
+  const children = Array.prototype.slice.call(node.content.childNodes);
+
+  parent.appendChild(node.content);
+
+  // Svelte except an used node to have parent, so a fragment should adopt the node
+  document.createDocumentFragment().appendChild(document.adoptNode(node));
+
+  return {
+    destroy() {
+      // Node content fragment doesnt longer have the references to the children
+      // so its content need to be removed reference by reference
+      requestAnimationFrame(function() {
+        for (let i in children) {
+          if (parent && parent.contains(children[i])) {
+            parent.removeChild(children[i]);
+          }
+        }
+      });
+    },
+  };
 }
